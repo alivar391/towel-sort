19e61fa0fbbe487b799a525bbd0d7e6803e5e6a2
alivar391 <patriot391@mail.ru>
2021-08-12 21:48:41 +0300

    feat: add function ti index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (arguments.length == 0) return [];
+    let result = [];
+    for (let i = 0; i < matrix.length; i++){
+        if (i % 2 === 0 || i === 0) {
+            result = result.concat(matrix[i]);
+        }
+        else result = result.concat(matrix[i].reverse());
+    }
+    console.log(result);
+    return result;
+    }
